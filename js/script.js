9b26b4837b4746f45c795c222507c514ce490743
wonejito <30444850+wonejito@users.noreply.github.com>
2023-02-20 11:46:17 -0400

    Well, this is awesome to me. i did it all :)

--- js/script.js
+++ js/script.js
@@ -2,21 +2,49 @@ let pantalla = document.querySelector("#little-screen");
 let numbers = document.querySelectorAll(".number");
 let operators = document.querySelectorAll(".operator");
 let clear = document.querySelector("#clear");
-
+let equal = document.querySelector("#equal");
+let clean = (pantalla.textContent = "");
 numbers.forEach((num) => {
 	num.addEventListener("click", function () {
-		pantalla.textContent += this.textContent;
+		justTyped = this.textContent;
+		pantalla.textContent += justTyped;
+		inDisplay = pantalla.textContent;
 	});
 });
 
 operators.forEach((operator) => {
 	operator.addEventListener("click", function () {
-		pantalla.textContent += this.textContent;
+		selectedOperator = this.textContent;
+		if (pantalla.textContent != this.textContent) {
+			pantalla.textContent += this.textContent;
+		} else {
+			pantalla.textContent;
+		}
 	});
 });
 
 clear.addEventListener("click", function () {
 	pantalla.textContent = "";
+});
+
+equal.addEventListener("click", function () {
+	let arrayFromDisplay = inDisplay.split("");
+	opr = arrayFromDisplay.filter((sgn) => sgn == selectedOperator);
+	//console.log(opr);
+	sameArray = inDisplay.split(`${opr}`).map(Number);
+	if (opr == "+") {
+		clean;
+		pantalla.textContent = add(sameArray[0], sameArray[1]);
+	} else if (opr == "-") {
+		clean;
+		pantalla.textContent = substract(sameArray[0], sameArray[1]);
+	} else if (opr == "x") {
+		clean;
+		pantalla.textContent = multiply(sameArray[0], sameArray[1]);
+	} else if ((opr = "/")) {
+		clean;
+		pantalla.textContent = divide(sameArray[0], sameArray[1]);
+	}
 });
 
 function add(num1, num2) {
