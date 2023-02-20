134f7198a7ffdc65b9098b3f420a6e8e2cc54a8d
wonejito <30444850+wonejito@users.noreply.github.com>
2023-02-20 09:11:13 -0400

    Yep

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+let pantalla = document.querySelector("#little-screen");
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".operator");
+let clear = document.querySelector("#clear");
+
+numbers.forEach((num) => {
+	num.addEventListener("click", function () {
+		pantalla.textContent += this.textContent;
+	});
+});
+
+operators.forEach((operator) => {
+	operator.addEventListener("click", function () {
+		pantalla.textContent += this.textContent;
+	});
+});
+
+clear.addEventListener("click", function () {
+	pantalla.textContent = "";
+});
+
 function add(num1, num2) {
 	return num1 + num2;
 }
@@ -17,4 +38,3 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
 	return operator(num1, num2);
 }
-
